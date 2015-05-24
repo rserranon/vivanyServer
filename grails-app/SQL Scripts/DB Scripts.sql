8207c57b2e112c5fbea901709a4f4c6e6ebd412e
Roberto Serrano <rserranon@gmail.com>
2015-05-24 14:38:28 -0500

    Minor changes to SQL commands

--- grails-app/SQL Scripts/DB Scripts.sql
+++ grails-app/SQL Scripts/DB Scripts.sql
@@ -1,15 +1,19 @@
 // MySql Connect
+
 mysql --host=localhost --user='startu60_serrano' --password='oGo-S84-RLR-ogf' vivany
 
 
 // Dump database
+
 mysqldump -u 'startu60_serrano' --password='oGo-S84-RLR-ogf' --databases 'vivany' > '/Users/proyectoweb/Documents/RSN/grails_apps/vivanyServer/grails-app/SQL Scripts/DBbackup.sql'
 
 // Export table to CSV
+
 select * from tablename   INTO OUTFILE  '/tmp/filename.csv'  FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
 
 
 // Create tb_data_icd Table
+
 CREATE TABLE IF NOT EXISTS `tb_data_icd` (
 `col1` int(1) DEFAULT NULL,
 `col2` varchar(1) DEFAULT NULL,
@@ -28,7 +32,8 @@ CREATE TABLE IF NOT EXISTS `tb_data_icd` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 // Load info from delimited file
-load data local infile '/tmp/icd102010enMeta/codes.txt'
+
+load data local infile '/Users/proyectoweb/Downloads/icd102010enMeta/codes.txt'
 into table tb_data_icd
 fields terminated by ';'
 enclosed by ';'
@@ -36,13 +41,8 @@ lines terminated by '\n'
 (col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12,col13,col14);
 
 
-INSERT INTO disease (version, disease_id, disease_name)  
-SELECT '0',col8, col9
-  FROM `tb_data_icd`
- WHERE `col2` = 'T'
-
-
 // Copy info into disease table
+
 INSERT INTO disease (version, disease_id, disease_name)  
 SELECT '0',col8, col9
   FROM `tb_data_icd`
